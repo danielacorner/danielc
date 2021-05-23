2c2f95bd9e576d9851d607305599baae6455eb87
Daniel Corner <danielcorner7@gmail.com>
2021-05-23 19:47:44 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,33 @@
+module.exports = {
+  siteMetadata: {
+    title: "danielc",
+  },
+  plugins: [
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-gatsby-cloud",
+    "gatsby-plugin-image",
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+    {
+      resolve: "gatsby-plugin-manifest",
+      options: {
+        icon: "src/images/icon.png",
+      },
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: "gatsby-source-filesystem",
+      options: {
+        name: "images",
+        path: "./src/images/",
+      },
+      __key: "images",
+    },
+  ],
+};
